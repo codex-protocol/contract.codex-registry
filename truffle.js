836db0a9f81558d9d0d1155e1460c9f9ce5b392a
John Forrest <johnhforrest@outlook.com>
2018-02-15 14:59:43 -0600

    Added Ganache option for local development

--- truffle.js
+++ truffle.js
@@ -1,4 +1,14 @@
-//require('babel-register')
-//require('babel-polyfill')
+// TODO: revert this file unless ganache is running
+// TODO: this is why some projects use scripts/test.sh
 
-module.exports = {}
+module.exports = {
+  /*
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*' // Match any network id
+    }
+  }
+  */
+}
