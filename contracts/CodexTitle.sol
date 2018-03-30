a0689de48b0d4c82889157608348d857b760b5a2
John Forrest <johnhforrest@outlook.com>
2018-03-30 14:54:35 -0500

    Fix logic error

--- contracts/CodexTitle.sol
+++ contracts/CodexTitle.sol
@@ -4,7 +4,30 @@ import "./zeppelin-solidity/token/ERC721/ERC721Token.sol";
 
 
 contract CodexTitle is ERC721Token {
-  function CodexTitle() public ERC721Token("Codex Title", "CT") {
+  struct CodexTitleData {
+    string name;
+    string description;
+    string imageUri;
+  }
 
+  mapping(uint256 => CodexTitleData) internal tokenData;
+
+  function CodexTitle() public ERC721Token("Codex Title", "CT")
+  { }
+
+  function mint(
+    address _to, string _name, string _description, string _imageUri) public {
+    uint256 tokenId = allTokens.length;
+
+    super._mint(_to, tokenId);
+
+    tokenData[tokenId] = CodexTitleData(_name, _description, _imageUri);
+  }
+
+  function getTokenById(uint256 _tokenId) external view
+    returns (string name, string description, string imageUri) {
+    CodexTitleData storage codexTitle = tokenData[_tokenId];
+
+    return (codexTitle.name, codexTitle.description, codexTitle.imageUri);
   }
 }
