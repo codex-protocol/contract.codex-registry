9635b2a0087d2cf79b16062426797eb5eb33b78d
John Forrest <johnhforrest@outlook.com>
2018-04-15 17:18:44 -0500

    Tests passing with proxy for ERC721BasicToken

--- test/ERC721/ERC721BasicToken.test.js
+++ test/ERC721/ERC721BasicToken.test.js
@@ -1,10 +1,8 @@
 // IMPORTANT NOTE: The web3 instance injected by Truffle is currently 0.2x.x
 // Documentation here: https://github.com/ethereum/wiki/wiki/JavaScript-API
 
-const abi = require('ethereumjs-abi');
-
-// import shouldBehaveLikeERC721BasicToken from './ERC721BasicToken.behaviour';
-// import shouldMintERC721Token from './ERC721Mint.behaviour';
+import shouldBehaveLikeERC721BasicToken from './ERC721BasicToken.behaviour';
+import shouldMintERC721Token from './ERC721Mint.behaviour';
 
 const BigNumber = web3.BigNumber;
 const ERC721BasicToken = artifacts.require('ERC721BasicTokenMock.sol');
@@ -27,8 +25,7 @@ contract('ERC721BasicToken', function (accounts) {
     const implementation = await proxy.implementation();
     implementation.should.be.equal(basicToken.address);
 
-    this.token = proxy;
-    this.originalToken = basicToken;
+    this.token = ERC721BasicToken.at(proxy.address);
   });
 
   describe('like a ERC721BasicToken', function () {
@@ -37,78 +34,27 @@ contract('ERC721BasicToken', function (accounts) {
     const creator = accounts[0];
 
     beforeEach(async function () {
-      // await this.token.mint(creator, firstTokenId, { from: creator });
-      // await this.token.mint(creator, secondTokenId, { from: creator });
-
-      let transactionData = abi.simpleEncode(
-        'mint(address,uint256)',
-        creator,
-        firstTokenId
-      );
-
-      await this.token.sendTransaction({
-        data: transactionData.toString('hex'),
-        from: creator,
-      });
-
-      transactionData = abi.simpleEncode(
-        'mint(address,uint256)',
-        creator,
-        secondTokenId
-      );
-
-      await this.token.sendTransaction({
-        data: transactionData.toString('hex'),
-        from: creator,
-      });
+      await this.token.mint(creator, firstTokenId, { from: creator });
+      await this.token.mint(creator, secondTokenId, { from: creator });
     });
 
     describe('balanceOf', function () {
       describe('when the given address owns some tokens', function () {
         it('returns the amount of tokens owned by the given address', async function () {
-          const transactionData = abi.simpleEncode(
-            'balanceOf(address)',
-            creator,
-          );
-
-          let balance = await this.token.sendTransaction({
-            data: transactionData.toString('hex'),
-            from: creator,
-          });
-
-          console.log(JSON.stringify(balance));
-
-          balance = await this.originalToken.sendTransaction({
-            data: transactionData.toString('hex'),
-            from: creator,
-          });
-
-          console.log(JSON.stringify(balance));
-
-          // const balance = await this.token.balanceOf(creator);
+          const balance = await this.token.balanceOf(creator);
           balance.should.be.bignumber.equal(2);
         });
       });
 
       describe('when the given address does not own any tokens', function () {
         it('returns 0', async function () {
-          const transactionData = abi.simpleEncode(
-            'balanceOf(address)',
-            accounts[1],
-          );
-
-          const balance = await this.token.sendTransaction({
-            data: transactionData.toString('hex'),
-            from: creator,
-          });
-
-          // const balance = await this.token.balanceOf(accounts[1]);
+          const balance = await this.token.balanceOf(accounts[1]);
           balance.should.be.bignumber.equal(0);
         });
       });
     });
   });
 
-  // shouldBehaveLikeERC721BasicToken(accounts);
-  // shouldMintERC721Token(accounts);
+  shouldBehaveLikeERC721BasicToken(accounts);
+  shouldMintERC721Token(accounts);
 });
