63b7fc9d167351a8d1a27722d1d5785f7951a29f
Shawn Price <shawnp@gmail.com>
2018-05-23 10:56:09 -0700

    Make contract pausable (#7)
    * SCRUM-220. Make mint() pausable
    
    * SCRUM-220. Make transferFrom and safeTransferFrom pausable
    
    * SCRUM-220. Make modifying functions public
    
    * SCRUM-220. Make modifying functions pausable
    
    * SCRUM-220. Add tests to paysable modifying functions.
    
    * Add initializeOwnable in test
    
    * Make pull request changes
    
    * Proper testing of bign umber
    
    * fix test

---
+++ contracts/CodexTitleAccess.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.4.23;
+
+import "./CodexTitleCore.sol";
+
+
+/**
+ * @title CodexTitleAccess
+ * @dev Override contract functions
+ */
+contract CodexTitleAccess is CodexTitleCore {
+
+  /**
+  * @dev Make mint() pausable
+  */
+  function mint(
+    address _to,
+    bytes32 _nameHash,
+    bytes32 _descriptionHash,
+    bytes32 _imageHash,
+    string _providerId, // TODO: convert to bytes32
+    string _providerMetadataId) // TODO: convert to bytes32
+    public
+    whenNotPaused
+  {
+    return super.mint(
+      _to,
+      _nameHash,
+      _descriptionHash,
+      _imageHash,
+      _providerId,
+      _providerMetadataId
+    );
+  }
+
+  /**
+  * @dev Make trasferFrom() pausable
+  */
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId)
+    public
+    whenNotPaused
+  {
+    return super.transferFrom(_from, _to, _tokenId);
+  }
+
+  /**
+  * @dev Make safeTrasferFrom() pausable
+  */
+  function safeTransferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId)
+    public
+    whenNotPaused
+  {
+    return super.safeTransferFrom(_from, _to, _tokenId);
+  }
+
+  /**
+  * @dev Make safeTrasferFrom() pausable
+  */
+  function safeTransferFrom(
+    address _from,
+    address _to,
+    uint256 _tokenId,
+    bytes _data)
+    public
+    whenNotPaused
+  {
+    return super.safeTransferFrom(
+      _from,
+      _to,
+      _tokenId,
+      _data
+    );
+  }
+
+  /**
+  * @dev Make modifyDescriptionHash() pausable
+  */
+  function modifyDescriptionHash(uint256 _tokenId, bytes32 _newDescriptionHash) public whenNotPaused {
+    return super.modifyDescriptionHash(_tokenId, _newDescriptionHash);
+  }
+
+  /**
+  * @dev Make modifyNameHash() pausable
+  */
+  function modifyNameHash(uint256 _tokenId, bytes32 _newNameHash) public whenNotPaused {
+    return super.modifyNameHash(_tokenId, _newNameHash);
+  }
+
+  /**
+  * @dev Make addNewImageHash() pausable
+  */
+  function addNewImageHash(uint256 _tokenId, bytes32 _imageHash) public whenNotPaused {
+    return super.addNewImageHash(_tokenId, _imageHash);
+  }
+}
---
+++ test/token/CodexTitleAccess.test.js
@@ -0,0 +1,175 @@
+import assertRevert from '../helpers/assertRevert'
+
+const { BigNumber } = web3
+const CodexTitle = artifacts.require('CodexTitle.sol')
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+contract('CodexTitleAccess', async function (accounts) {
+  const creator = accounts[0]
+  const another = accounts[1]
+  const firstTokenId = 0
+  const providerId = '1'
+  const providerMetadataId = '10'
+
+  const firstTokenMetadata = {
+    name: 'First token',
+    description: 'This is the first token',
+    imageBytes: 'asdf',
+  }
+
+  const hashedMetadata = {
+    name: web3.sha3(firstTokenMetadata.name),
+    description: web3.sha3(firstTokenMetadata.description),
+    imageBytes: web3.sha3(firstTokenMetadata.imageBytes),
+  }
+
+  beforeEach(async function () {
+    this.token = await CodexTitle.new()
+    await this.token.initializeOwnable(creator)
+
+    await this.token.mint(
+      creator,
+      hashedMetadata.name,
+      hashedMetadata.description,
+      hashedMetadata.imageBytes,
+      providerId,
+      providerMetadataId,
+    )
+  })
+
+  describe('mint', function () {
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.mint(
+            creator,
+            hashedMetadata.name,
+            hashedMetadata.description,
+            hashedMetadata.imageBytes,
+            providerId,
+            providerMetadataId,
+          )
+        )
+      })
+    })
+  })
+
+  describe('transferFrom', function () {
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.transferFrom(creator, another, firstTokenId)
+        )
+      })
+    })
+
+    describe('when contract unpaused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('allows transfer', async function () {
+        await this.token.unpause()
+        await this.token.transferFrom(creator, another, firstTokenId, { from: creator })
+        const countAnother = await this.token.balanceOf(another)
+        countAnother.should.be.bignumber.equal(1)
+        const countCreator = await this.token.balanceOf(creator)
+        countCreator.should.be.bignumber.equal(0)
+      })
+    })
+  })
+
+  describe('safeTransferFrom', function () {
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.safeTransferFrom(creator, another, firstTokenId)
+        )
+      })
+    })
+  })
+
+  describe('safeTransferFrom with data', function () {
+    const data = new Uint32Array(10)
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.safeTransferFrom(creator, another, firstTokenId, data)
+        )
+      })
+    })
+  })
+
+  describe('modifyDescriptionHash', function () {
+    const data = web3.sha3('description')
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.modifyDescriptionHash(firstTokenId, data)
+        )
+      })
+    })
+  })
+
+  describe('modifyNameHash', function () {
+    const data = web3.sha3('name')
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.modifyNameHash(firstTokenId, data, { from: creator })
+        )
+      })
+    })
+  })
+
+  describe('addNewImageHash', function () {
+    const data = web3.sha3('image')
+    describe('when contract paused', function () {
+      beforeEach(async function () {
+        // Pause the contract
+        await this.token.pause()
+      })
+
+      it('should revert', async function () {
+        await assertRevert(
+          this.token.addNewImageHash(firstTokenId, data, { from: creator })
+        )
+      })
+    })
+  })
+})
