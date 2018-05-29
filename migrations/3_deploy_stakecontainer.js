e1c52f8ad5d86333bc0feb020d52865227a03914
John Forrest <johnhforrest@outlook.com>
2018-05-29 17:09:35 -0500

    Fix build

--- migrations/3_deploy_stakecontainer.js
+++ migrations/3_deploy_stakecontainer.js
@@ -1,6 +1,6 @@
 const CodexToken = artifacts.require('./CodexToken.sol')
-const ERC900StakeContainer = artifacts.require('./ERC900StakeContainer.sol')
+const ERC900BasicStakeContainer = artifacts.require('./ERC900BasicStakeContainer.sol')
 
 module.exports = (deployer) => {
-  deployer.deploy(ERC900StakeContainer, CodexToken.address)
+  deployer.deploy(ERC900BasicStakeContainer, CodexToken.address)
 }
