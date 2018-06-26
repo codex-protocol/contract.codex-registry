7969c24e248985cb8b31f64e78e47ea6c7421615
John Forrest <johnhforrest@outlook.com>
2018-06-26 16:39:22 -0500

    Gas optimization when removing tokens

--- contracts/CodexRecord.sol
+++ contracts/CodexRecord.sol
@@ -15,4 +15,13 @@ contract CodexRecord is CodexRecordAccess {
    * @dev Constructor function
    */
   constructor() public ERC721Token("Codex Record", "CR") { }
+
+  /**
+   * @dev Reclaim all ERC20Basic compatible tokens
+   * @param token ERC20Basic The address of the token contract
+   */
+  function reclaimToken(ERC20Basic token) external onlyOwner {
+    uint256 balance = token.balanceOf(this);
+    token.transfer(owner, balance);
+  }
 }
--- contracts/ERC721/ERC721Token.sol
+++ contracts/ERC721/ERC721Token.sol
@@ -136,12 +136,10 @@ contract ERC721Token is ERC721, ERC721BasicToken {
     uint256 lastTokenIndex = ownedTokens[_from].length.sub(1);
     uint256 lastToken = ownedTokens[_from][lastTokenIndex];
 
-    ownedTokens[_from][tokenIndex] = lastToken;
-    ownedTokens[_from][lastTokenIndex] = 0;
     // Note that this will handle single-element arrays. In that case, both tokenIndex and lastTokenIndex are going to
     // be zero. Then we can make sure that we will remove _tokenId from the ownedTokens list since we are first swapping
     // the lastToken to the first position, and then dropping the element placed in the last position of the list
-
+    ownedTokens[_from][tokenIndex] = lastToken;
     ownedTokens[_from].length--;
     ownedTokensIndex[_tokenId] = 0;
     ownedTokensIndex[lastToken] = tokenIndex;
