72abc05f9bef9238a87e8a645ce80fc80bd97483
John Forrest <johnhforrest@outlook.com>
2018-06-28 18:00:50 -0500

    Fix failing tests

--- test/helpers/convertDataToBytes.js
+++ test/helpers/convertDataToBytes.js
@@ -1,11 +1,12 @@
 const convertDataToBytes = (data) => {
+  const numColons = 2
   let concatenatedString = ''
 
   Object.values(data).forEach((value) => {
     concatenatedString += `${value}::`
   })
 
-  const buffer = Buffer.from(concatenatedString.substring(0, concatenatedString.length - 3))
+  const buffer = Buffer.from(concatenatedString.substring(0, concatenatedString.length - numColons))
   return `0x${buffer.toString('hex')}`
 }
 
