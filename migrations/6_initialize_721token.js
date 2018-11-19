c025c40f5b99cc346405ffc149f7a1c9cd2ebc1f
John Forrest <johnhforrest@outlook.com>
2018-11-19 16:26:19 -0600

    Comment out unecessary migration steps to save gas

--- migrations/6_initialize_721token.js
+++ migrations/6_initialize_721token.js
@@ -1,52 +1,52 @@
 const CodexCoin = artifacts.require('./CodexCoin.sol')
 const CodexRecord = artifacts.require('./CodexRecord.sol')
 const CodexRecordProxy = artifacts.require('./CodexRecordProxy.sol')
-const CodexStakeContract = artifacts.require('./CodexStakeContract.sol')
+// const CodexStakeContract = artifacts.require('./CodexStakeContract.sol')
 
 module.exports = async (deployer, network, accounts) => {
   const proxiedCodexRecord = CodexRecord.at(CodexRecordProxy.address)
 
   deployer
     .then(async () => {
-      let initialFees
-      let erc20TokenAddress
+      // let initialFees
+      // let erc20TokenAddress
 
-      switch (network) {
-        case 'test':
-        case 'ganache':
-        case 'develop':
-        case 'coverage':
-          // initialFees = web3.toWei(1, 'ether')
-          initialFees = 0
-          erc20TokenAddress = CodexCoin.address
-          break
+      // switch (network) {
+      //   case 'test':
+      //   case 'ganache':
+      //   case 'develop':
+      //   case 'coverage':
+      //     // initialFees = web3.toWei(1, 'ether')
+      //     initialFees = 0
+      //     erc20TokenAddress = CodexCoin.address
+      //     break
 
-        case 'ropsten':
-          initialFees = 0
-          erc20TokenAddress = '0x2af5409438d2e6c241015f3130213f6a122b4064'
-          break
+      //   case 'ropsten':
+      //     initialFees = 0
+      //     erc20TokenAddress = '0x2af5409438d2e6c241015f3130213f6a122b4064'
+      //     break
 
-        case 'rinkeby':
-          initialFees = 0
-          erc20TokenAddress = '0x6a8c5db1495ffc4ef183dfccfdc4de5164b4e95c'
-          break
+      //   case 'rinkeby':
+      //     initialFees = 0
+      //     erc20TokenAddress = '0x6a8c5db1495ffc4ef183dfccfdc4de5164b4e95c'
+      //     break
 
-        default:
-          throw new Error('No erc20TokenAddress & initialFees defined for this network')
-      }
+      //   default:
+      //     throw new Error('No erc20TokenAddress & initialFees defined for this network')
+      // }
 
-      console.log(`Setting the fees to ${initialFees} at ERC-20 token address: ${erc20TokenAddress}`)
-      await proxiedCodexRecord.setFees(
-        erc20TokenAddress,
-        accounts[0],
-        initialFees, // creationFee
-        initialFees, // transferFee
-        initialFees, // modificationFee
-      )
+      // console.log(`Setting the fees to ${initialFees} at ERC-20 token address: ${erc20TokenAddress}`)
+      // await proxiedCodexRecord.setFees(
+      //   erc20TokenAddress,
+      //   accounts[0],
+      //   initialFees, // creationFee
+      //   initialFees, // transferFee
+      //   initialFees, // modificationFee
+      // )
 
-      await proxiedCodexRecord.setStakeContract(
-        CodexStakeContract.address
-      )
+      // await proxiedCodexRecord.setStakeContract(
+      //   CodexStakeContract.address
+      // )
 
       if (network === 'ganache') {
         const codexRecordAddress = proxiedCodexRecord.address
